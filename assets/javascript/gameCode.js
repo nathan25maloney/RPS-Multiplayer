26589264a531e81d1b183d81a1eb021b749f436b
Nathan Maloney <Nathan Maloney>
2017-05-24 21:29:37 -0700

    got the message board working

--- assets/javascript/gameCode.js
+++ assets/javascript/gameCode.js
@@ -66,41 +66,91 @@ function newPlayer() {
 	}
 }
 
-database.ref("players").on("value", function(snapshot) {
-	isPlayer1 = snapshot.val().isPlayer1;
-	isPlayer2 = snapshot.val().isPlayer2;
-})
 
-database.ref("player1").on("value", function(snapshot) {
-	player1name = snapshot.val().name;
-	player1wins = snapshot.val().wins;
-	player1loses = snapshot.val().loses;
-	text = snapshot.val().text;
-	$("#player-1-name").text(player1name);
-	$("#player-1-wins").text(player1wins);
-	$("#player-1-loses").text(player1loses);
-	$("#winner").text(text);
-})
-
-database.ref("player2").on("value", function(snapshot) {
-	player2name = snapshot.val().name;
-	player2wins = snapshot.val().wins;
-	player2loses = snapshot.val().loses;
-	text = snapshot.val().text;
-	$("#player-2-name").text(player2name);
-	$("#player-2-wins").text(player2wins);
-	$("#player-2-loses").text(player2loses);
-	$("#winner").text(text);
-
-})
 
 
 $(function () {
+	var messagesRef = database.ref("chat");
+
 	$("#new-player").on("click", function(e) {
 	e.preventDefault();
 	console.log("The button is working");
 	newPlayer();
 	});
+	$("#name-input").keyup(function (e) {
+    if (e.keyCode == 13) {	
+      e.preventDefault();
+      $("#new-player").click();  
+    };
+	});
+
+	$("#Rock").on("click", function () {
+		run("rock");
+	});
+	$("#Paper").on("click", function () {
+		run("paper");
+	});
+	$("#Scissors").on("click", function () {
+		run("scissors");
+	});
+	$("#Lizard").on("click", function () {
+		run("lizard");
+	});
+	$("#Spock").on("click", function () {
+		run("spock");
+	});
+	$("#submitmsg").on("click", function (e) {
+		e.preventDefault();
+		var name = nameHolder;
+	    var text = $("#usermsg").val();
+	    messagesRef.push({name:name, text:text});
+	    $("#usermsg").val("");
+
+	});
+
+  $("#usermsg").keyup(function (e) {
+    if (e.keyCode == 13) {	
+      e.preventDefault();
+      $("#submitmsg").click();
+      
+    }
+  });
+
+
+  messagesRef.limitToLast(10).on("child_added", function (snapshot) {
+    var message = snapshot.val();
+    $("<div/>").text(message.text).prepend($("<em/>")
+      .text(message.name + ": ")).appendTo($("#chatbox"));
+    $("#chatbox")[0].scrollTop = $("#chatbox")[0].scrollHeight;
+  });
+
+    database.ref("players").on("value", function(snapshot) {
+		isPlayer1 = snapshot.val().isPlayer1;
+		isPlayer2 = snapshot.val().isPlayer2;
+	})
+
+	database.ref("player1").on("value", function(snapshot) {
+		player1name = snapshot.val().name;
+		player1wins = snapshot.val().wins;
+		player1loses = snapshot.val().loses;
+		text = snapshot.val().text;
+		$("#player-1-name").text(player1name);
+		$("#player-1-wins").text(player1wins);
+		$("#player-1-loses").text(player1loses);
+		$("#winner").text(text);
+	})
+
+	database.ref("player2").on("value", function(snapshot) {
+		player2name = snapshot.val().name;
+		player2wins = snapshot.val().wins;
+		player2loses = snapshot.val().loses;
+		text = snapshot.val().text;
+		$("#player-2-name").text(player2name);
+		$("#player-2-wins").text(player2wins);
+		$("#player-2-loses").text(player2loses);
+		$("#winner").text(text);
+
+	})
 
 
 
@@ -258,24 +308,6 @@ function check(firstarg, secondarg) {
 	}
 }
 
-$(document).ready(function(){
-	$("#Rock").on("click", function () {
-		run("rock");
-	});
-	$("#Paper").on("click", function () {
-		run("paper");
-	});
-	$("#Scissors").on("click", function () {
-		run("scissors");
-	});
-	$("#Lizard").on("click", function () {
-		run("lizard");
-	});
-	$("#Spock").on("click", function () {
-		run("spock");
-	});
 
-
-});
 
 
